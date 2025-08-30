d01f93ac2ee4ea084c1be73d3fede16f41811dfe
AdityaRaj81 <Connect2RajAditya@gmail.com>
2025-08-30 22:52:06 +0530

    first commit

---
+++ src/First.java
@@ -0,0 +1,7 @@
+public class First {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        System.out.println("Welcome to Java Programming!");
+        System.out.println("Let's learn Java together!");
+    }
+}

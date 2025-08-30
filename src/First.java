41b43654b9678359c89fe7e3a46cef0fbd716640
AdityaRaj81 <Connect2RajAditya@gmail.com>
2025-08-30 22:55:48 +0530

    Add VCS configuration and update First.java with error message

--- src/First.java
+++ src/First.java
@@ -3,5 +3,6 @@ public class First {
         System.out.println("Hello, World!");
         System.out.println("Welcome to Java Programming!");
         System.out.println("Let's learn Java together!");
+        System.err.println("This is an error message.");
     }
 }
